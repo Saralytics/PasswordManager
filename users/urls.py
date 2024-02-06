9b85d209841f9bb91869712a4194b5c97ea5d8c6
Saralytics <61179765+Saralytics@users.noreply.github.com>
2024-02-06 22:36:34 +0400

    Revert "Tested api with auth token"

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from . import views
 
 app_name = "users"
@@ -7,6 +7,4 @@ urlpatterns = [
     path("users/", views.user_list, name="userlist"),
     path("register/", views.user_register, name="register"),
     path("auth/", views.login_view, name="auth"),
-    path("token/",TokenObtainPairView.as_view(), name="token_obtain"),
-    path("token/refresh",TokenRefreshView.as_view(), name="token_refresh"),
 ]
