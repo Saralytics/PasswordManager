92a42a4db879bedd397a7547ac628274eae94129
Saralytics <zhaoxue.li@outlook.com>
2024-02-12 20:57:36 +0400

    created and tested the feature to store new passwords

--- backend/vault/views.py
+++ backend/vault/views.py
@@ -5,9 +5,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.tokens import RefreshToken
 from django.middleware import csrf
 from django.conf import settings
 
