a8ac758faf022e5907164eaf16917786b3dfac30
Saralytics <zhaoxue.li@outlook.com>
2024-03-17 07:40:58 +0400

    display errors on ui when login input is invalid

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -6,18 +6,28 @@ import { useNavigate } from 'react-router-dom';
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
   const { login } = useAuth();
   let navigate = useNavigate();
 
+  const validate = () => {
+    let tempErrors = {};
+    tempErrors.username = username ? "" : "Username is required";
+    tempErrors.password = password ? "" : "Password is required";
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every(x => x === "");
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if(!validate()) return;
+
     try {
       await login(username, password);
       navigate('/new-password'); // Redirect to a protected page or dashboard
       console.log('Login successful');
     } catch (error) {
-      // alert('Login failed');
-      console.log(error);
+      alert('Username or password is incorrect');
     }
   };
 
@@ -31,6 +41,7 @@ const LoginPage = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+        {errors.username && <p>{errors.username}</p>}
       </div>
       <div>
         <label htmlFor="password">Password:</label>
@@ -40,6 +51,7 @@ const LoginPage = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        {errors.password && <p>{errors.password}</p>}
       </div>
       <button type="submit">Login</button>
     </form>
