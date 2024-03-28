8f2e7464100f46a3c734b31c25f261eb0a34202a
Saralytics <zhaoxue.li@outlook.com>
2024-03-28 21:59:29 +0400

    completed the list password view

--- frontend/src/pages/ListVault.js
+++ frontend/src/pages/ListVault.js
@@ -5,6 +5,8 @@ import { getCookie } from '../utils/GetCookie';
 
 function ListVault() {
     const csrfToken = getCookie('csrftoken');
+    const [vault, setVault] = useState([]);
+    const [apiError, setApiError ] = useState('');
 
     // function calls api
     const handleSubmit = async (e) => {
@@ -16,10 +18,12 @@ function ListVault() {
               'X-CSRFToken': csrfToken,
             }
           });
-          console.log(response)
+          console.log(response.data['vault']);
+          setVault(response.data['vault']);
 
         } catch (error) {
             console.error('Error occured while searching password:', error.response ? error.response.data : 'Unknown error');
+            setApiError(error.response.data);
         };
 
     }; 
@@ -27,12 +31,26 @@ function ListVault() {
     return (
         <form onSubmit={handleSubmit}>
       
-      <div>
-        <label htmlFor="website">Website URL:</label>
-      </div>
+        <div>
+            <label htmlFor="website">Website URL:</label>
+            <h1>Vault</h1>
+            <ul>
+                {vault.map((item) => (
+                <li key={item.id}>
+                    <div>Website: {item.website}</div>
+                    <div>Username: {item.username}</div>
+                    <div>Password: {item.password}</div>
+                    <div>Created At: {item.created_at}</div>
+                    <div>Updated At: {item.updated_at}</div>
+                </li>
+                ))}
+            </ul>
+        </div>
       
-      <button type="submit">List</button>
+        <button type="submit">List</button>
+        {apiError}
     </form>
+    
     );
 
 }
